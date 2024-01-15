5cdd91678c9df51caf4e47376594541b2db13fdd
rasheed1026 <130817368+rasheedLPU@users.noreply.github.com>
2024-01-15 12:45:25 +0530

    Add files via upload

---
+++ ScrollView/Scrollview.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication123
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scrollview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrollview)
+    }
+}
